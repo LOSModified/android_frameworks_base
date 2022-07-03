98c9437652b542cae08f6f4eb71a681f15ffc952
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-07-03 00:08:42 +0000

    AppStandbyController: Disable "RESTRICTED bucket" toast
    Change-Id: I20a328d3c77962f3a6095bfca42d0b165a093ce8

--- apex/jobscheduler/service/java/com/android/server/usage/AppStandbyController.java
+++ apex/jobscheduler/service/java/com/android/server/usage/AppStandbyController.java
@@ -1874,25 +1874,7 @@ public class AppStandbyController
                         .noteRestrictionAttempt(packageName, userId, elapsedRealtime, reason);
 
                 if (isForcedByUser) {
-                    // Only user force can bypass the delay restriction. If the user forced the
-                    // app into the RESTRICTED bucket, then a toast confirming the action
-                    // shouldn't be surprising.
-                    // Exclude REASON_SUB_FORCED_USER_FLAG_INTERACTION since the RESTRICTED bucket
-                    // isn't directly visible in that flow.
-                    if (Build.IS_DEBUGGABLE
-                            && (reason & REASON_SUB_MASK)
-                            != REASON_SUB_FORCED_USER_FLAG_INTERACTION) {
-                        Toast.makeText(mContext,
-                                // Since AppStandbyController sits low in the lock hierarchy,
-                                // make sure not to call out with the lock held.
-                                mHandler.getLooper(),
-                                mContext.getResources().getString(
-                                        R.string.as_app_forced_to_restricted_bucket, packageName),
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    } else {
                         Slog.i(TAG, packageName + " restricted by user");
-                    }
                 } else {
                     final long timeUntilRestrictPossibleMs = app.lastUsedByUserElapsedTime
                             + mInjector.getAutoRestrictedBucketDelayMs() - elapsedRealtime;
