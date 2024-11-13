8cbd874dfceb48374bb5fe4ed5cacbb035253c48
Danny Lin <danny@kdrag0n.dev>
2024-11-13 15:14:42 +0800

    SystemUI: Use new privacy indicators for location
    Android has had location indicators for a while, but let's use the new
    privacy indicator infrastructure for displaying them. This makes them
    integrate better with the new camera and microphone indicators.
    
    Change-Id: Ie457bb2dad17bddbf9dc3a818e3ec586023ce918

--- packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
+++ packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
@@ -67,7 +67,7 @@ class PrivacyItemController @Inject constructor(
     val micCameraAvailable
         get() = privacyConfig.micCameraAvailable
     val locationAvailable
-        get() = privacyConfig.locationAvailable
+        get() = true
     val allIndicatorsAvailable
         get() = micCameraAvailable && locationAvailable && privacyConfig.mediaProjectionAvailable
 
