7a19cebdcfa44e99b4db4c6041e788b514352d9c
junklu <quic_junklu@quicinc.com>
2025-05-07 15:29:11 +0800

    SystemUI: Reset when sleeping state change
    Reset to show/hide bouncer when sleeping state change.
    As dozing signal may be skipped on power button
    double press.
    
    Fixes: 413156280
    Test: manual - hit power button twice when on SIM unlock
    Google: 3618014
    Change-Id: I86ef82d5eea537fbd999da6b8759ea73de313806

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -1099,6 +1099,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     public void onStartedWakingUp() {
         mIsSleeping = false;
         setRootViewAnimationDisabled(false);
+        reset(false);
         NavigationBarView navBarView = mCentralSurfaces.getNavigationBarView();
         if (navBarView != null) {
             navBarView.forEachView(view ->
@@ -1118,6 +1119,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     public void onStartedGoingToSleep() {
         mIsSleeping = true;
         setRootViewAnimationDisabled(true);
+        reset(true);
         NavigationBarView navBarView = mCentralSurfaces.getNavigationBarView();
         if (navBarView != null) {
             navBarView.forEachView(view ->
