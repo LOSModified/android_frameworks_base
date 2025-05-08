83805b63c94ef53de425aaeb0c667c40c28ca5c7
TheHardGamer <gamelovr695@gmail.com>
2025-05-09 02:02:36 +0800

    jobscheduler: Disable debug

--- apex/jobscheduler/service/java/com/android/server/job/JobSchedulerService.java
+++ apex/jobscheduler/service/java/com/android/server/job/JobSchedulerService.java
@@ -178,8 +178,8 @@ import java.util.function.Predicate;
 public class JobSchedulerService extends com.android.server.SystemService
         implements StateChangedListener, JobCompletedListener {
     public static final String TAG = "JobScheduler";
-    public static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
-    public static final boolean DEBUG_STANDBY = DEBUG || false;
+    public static final boolean DEBUG = false;
+    public static final boolean DEBUG_STANDBY = false;
 
     public static final String TRACE_TRACK_NAME = "JobScheduler";
 
