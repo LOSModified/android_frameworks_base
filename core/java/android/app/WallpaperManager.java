4db49a1132c1fbbbb8e4539daedf070e2a69efc4
minaripenguin <minaripenguin@users.noreply.github.com>
2025-05-10 00:28:12 +0800

    WallpaperManager: Switch to WEBP_LOSSY

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -2285,7 +2285,7 @@ public class WallpaperManager {
                 FileOutputStream fos = null;
                 try {
                     fos = new ParcelFileDescriptor.AutoCloseOutputStream(fd);
-                    fullImage.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                    fullImage.compress(Bitmap.CompressFormat.WEBP_LOSSY, 90, fos);
                     fos.close();
                     completion.waitForCompletion();
                 } finally {
@@ -2344,7 +2344,7 @@ public class WallpaperManager {
                 FileOutputStream fos = null;
                 try {
                     fos = new ParcelFileDescriptor.AutoCloseOutputStream(fd);
-                    fullImage.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                    fullImage.compress(Bitmap.CompressFormat.WEBP_LOSSY, 90, fos);
                     fos.close();
                     completion.waitForCompletion();
                 } finally {
