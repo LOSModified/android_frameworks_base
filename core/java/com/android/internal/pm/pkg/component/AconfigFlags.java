f267a2866657c72322473357c8ebc942ef67d583
Dmitrii <bankersenator@gmail.com>
2026-01-20 00:27:26 +0000

    AconfigFlags: Silence missing packages spam
    E AconfigFlags: Failed to load aconfig package android.xr
    E AconfigFlags: android.os.flagging.AconfigStorageReadException: ERROR_PACKAGE_NOT_FOUND: package android.xr cannot be found on the device
    E AconfigFlags: 	at android.os.flagging.AconfigPackage.load(AconfigPackage.java:160)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.lambda$getFlagValueFromNewStorage$0(AconfigFlags.java:265)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags$$ExternalSyntheticLambda0.apply(D8$$SyntheticClass:0)
    E AconfigFlags: 	at java.util.concurrent.ConcurrentHashMap.computeIfAbsent(ConcurrentHashMap.java:1713)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.getFlagValueFromNewStorage(AconfigFlags.java:263)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.getFlagValue(AconfigFlags.java:229)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.skip(AconfigFlags.java:343)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.skipCurrentElement(AconfigFlags.java:331)
    E AconfigFlags: 	at com.android.internal.pm.pkg.component.AconfigFlags.skipCurrentElement(AconfigFlags.java:299)
    E AconfigFlags: 	at com.android.internal.pm.pkg.parsing.ParsingPackageUtils.parseBaseApkTags(ParsingPackageUtils.java:1053)
    E AconfigFlags: 	at com.android.internal.pm.pkg.parsing.ParsingPackageUtils.parseBaseApk(ParsingPackageUtils.java:786)
    E AconfigFlags: 	at com.android.internal.pm.pkg.parsing.ParsingPackageUtils.parseBaseApk(ParsingPackageUtils.java:618)
    E AconfigFlags: 	at com.android.internal.pm.pkg.parsing.ParsingPackageUtils.parseMonolithicPackage(ParsingPackageUtils.java:454)
    E AconfigFlags: 	at com.android.internal.pm.pkg.parsing.ParsingPackageUtils.parsePackage(ParsingPackageUtils.java:352)
    E AconfigFlags: 	at com.android.internal.pm.parsing.PackageParser2.parsePackage(PackageParser2.java:134)
    E AconfigFlags: 	at com.android.server.pm.ParallelPackageParser.parsePackage(go/retraceme c4881300f9a26cec87a4f45b7feae07c123ca539ee52fe36a528a87e7b5f9fc3:4)
    E AconfigFlags: 	at com.android.server.pm.ParallelPackageParser$$ExternalSyntheticLambda1.call(go/retraceme c4881300f9a26cec87a4f45b7feae07c123ca539ee52fe36a528a87e7b5f9fc3:47)
    E AconfigFlags: 	at java.util.concurrent.FutureTask.run(FutureTask.java:317)
    E AconfigFlags: 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1154)
    E AconfigFlags: 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:652)
    E AconfigFlags: 	at com.android.internal.util.ConcurrentUtils$1$1.run(ConcurrentUtils.java:65)
    E AconfigFlags: Failed to load aconfig package android.xr
    E AconfigFlags: android.os.flagging.AconfigStorageReadException: ERROR_PACKAGE_NOT_FOUND: package android.xr cannot be found on the device
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
+++ core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
@@ -264,7 +264,6 @@ public class AconfigFlags {
             try {
                 return AconfigPackage.load(p);
             } catch (Exception e) {
-                Slog.e(LOG_TAG, "Failed to load aconfig package " + p, e);
                 return null;
             }
         });
